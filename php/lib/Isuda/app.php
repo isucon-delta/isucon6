2396be8c53de76498e83a4bf03b5c32c12997957
Fumiya KARASAWA <fkarasaw@yahoo-corp.jp>
2016-09-18 12:52:34 +0900

    rm isutar

--- php/lib/Isuda/app.php
+++ php/lib/Isuda/app.php
@@ -114,9 +114,39 @@ $app->get('/initialize', function (Request $req, Response $c) {
     $this->dbh->query(
         'DELETE FROM entry WHERE id > 7101'
     );
-    $origin = config('isutar_origin');
-    $url = "$origin/initialize";
-    file_get_contents($url);
+    $this->dbh->query('TRUNCATE star');
+    return render_json($c, [
+        'result' => 'ok',
+    ]);
+});
+
+$app->get('/stars', function (Request $req, Response $c) {
+    $stars = $this->dbh->select_all(
+        'SELECT * FROM star WHERE keyword = ?'
+    , $req->getParams()['keyword']);
+
+    return render_json($c, [
+        'stars' => $stars,
+    ]);
+});
+
+$app->post('/stars', function (Request $req, Response $c) {
+    $keyword = $req->getParams()['keyword'];
+
+    $origin = $_ENV['ISUDA_ORIGIN'] ?? 'http://localhost:5000';
+    $url = "$origin/keyword/" . rawurlencode($keyword);
+    $ua = new \GuzzleHttp\Client;
+    try {
+        $res = $ua->request('GET', $url)->getBody();
+    } catch (\Exception $e) {
+        return $c->withStatus(404);
+    }
+
+    $this->dbh->query(
+        'INSERT INTO star (keyword, user_name, created_at) VALUES (?, ?, NOW())',
+        $keyword,
+        $req->getParams()['user']
+    );
     return render_json($c, [
         'result' => 'ok',
     ]);
